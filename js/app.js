4d686520e52708a2d20a01fda7176f46caa9deee
Sunny Lam <sunny.lam@gmail.com>
2015-06-28 19:45:02 -0400

    Add photo details using another .when() piped command

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ app.config(function ($routeProvider) {
       controller: 'HomeController', 
       templateUrl: 'views/home.html' 
     }) 
+    .when('/photos/:id',{
+    	// maps the URL /photos/:id to the controller PhotoController and the template views/photo.html
+    	// visit localhost... click on a photo. A detail page about that photo should appear
+    	controller: 'PhotoController', 
+    	templateUrl: 'views/photo.html' 
+    })
     .otherwise({ 
       redirectTo: '/' 
     }); 
